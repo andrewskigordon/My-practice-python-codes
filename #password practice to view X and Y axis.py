8ce63b85f1d56d3853ff04399b1a9e08f6dfdea2
Andrew Gordon <136789581+andrewskigordon@users.noreply.github.com>
2023-08-21 21:28:15 -0400

    Add files via upload

---
+++ #password practice to view X and Y axis.py
@@ -0,0 +1,24 @@
+#security practice questions
+
+string1 = input("what was the color of your dog in high school?")
+
+while not string1 == "BLUE":
+    print("Wrong entry")
+    string1 = input("what was the color of your dog in high school?")
+print("thanks for entering")
+
+string2 = input("what was the name if your taxi driver in NYC?")
+
+while not string2 == "WAZA":
+    print("wrong entry: ")
+    string2 = input("please enter your second password: ")
+print("thanks for entering\npress any button to continue: ")
+
+string3 = input("what is the name of your friend from school?: ")
+
+while not string3 == "JAKE":
+    print("wrong enetry: ")
+    string3 = input("what is the name of your friend from school?: ")
+print("thanks for enetring")
+
+
